0902c078c54c562ca697c863b13cbf49ecb57f31
Callum Hussey <callumhussey1995@gmail.com>
2019-06-19 22:38:45 +1200

    Added Menu

--- SudokuUI/SudokuUI/App.xaml.cs
+++ SudokuUI/SudokuUI/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
+using BackEnd;
 
 namespace SudokuUI
 {
@@ -15,8 +16,15 @@ namespace SudokuUI
 		{
 			InitializeComponent();
 
-			MainPage = new NavigationPage(new MainMenu());
-		}
+            MainPage = new NavigationPage(new MainMenu())
+            {
+                Title = "OcrSudoku",
+                BarBackgroundColor = Color.Red
+            };
+            //Board board = new BackEnd.Board();
+            //board.Fill("5,3,8,0,1,6,0,7,9,0,0,0,3,8,0,5,4,1,2,4,1,5,0,0,0,0,0,0,6,0,9,0,0,0,0,0,0,0,0,0,3,5,0,9,0,0,9,0,0,0,4,0,0,2,6,0,0,2,0,0,9,3,0,1,2,9,0,4,0,0,5,0,0,5,4,6,9,0,0,0,8");
+            //MainPage = new NavigationPage(new MainPage(board));
+        }
 
         public App(string DB_Path)
         {
@@ -24,7 +32,13 @@ namespace SudokuUI
 
             DB_PATH = DB_Path;
 
-            MainPage = new NavigationPage(new MainMenu());
+            MainPage = new NavigationPage(new MainMenu())
+            {
+                Title = "OcrSudoku"
+            };
+            //Board board = new BackEnd.Board();
+            //board.Fill("5,3,8,0,1,6,0,7,9,0,0,0,3,8,0,5,4,1,2,4,1,5,0,0,0,0,0,0,6,0,9,0,0,0,0,0,0,0,0,0,3,5,0,9,0,0,9,0,0,0,4,0,0,2,6,0,0,2,0,0,9,3,0,1,2,9,0,4,0,0,5,0,0,5,4,6,9,0,0,0,8");
+            //MainPage = new NavigationPage(new MainPage(board));
         }
 
 		protected override void OnStart ()
---
+++ SudokuUI/SudokuUI/MainMenu.xaml.cs
@@ -0,0 +1,28 @@
+﻿using BackEnd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SudokuUI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainMenu : ContentPage
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private async void Continue(object sender, EventArgs e)
+        {
+            Board board = new BackEnd.Board();
+            board.Fill("5,3,8,0,1,6,0,7,9,0,0,0,3,8,0,5,4,1,2,4,1,5,0,0,0,0,0,0,6,0,9,0,0,0,0,0,0,0,0,0,3,5,0,9,0,0,9,0,0,0,4,0,0,2,6,0,0,2,0,0,9,3,0,1,2,9,0,4,0,0,5,0,0,5,4,6,9,0,0,0,8");
+            await Navigation.PushAsync(new MainPage(board));
+        }
+    }
+}
